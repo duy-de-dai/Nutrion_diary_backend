88d5cd66db0922ecebeb51fcf93aff1b3077e7e8
duy-de-dai <maivuduy2k2@gmail.com>
2024-01-14 22:55:30 +0700

    update

--- src/knex/migrations/3_mealsDB.js
+++ src/knex/migrations/3_mealsDB.js
@@ -2,7 +2,7 @@ exports.up = (knex)=>{
     return knex.schema.createTable('meals', (table) => {
         table.increments('id').primary();
         table.string('name');
-        table.integer('time');
+        table.date('time');
         table.integer('user_id').unsigned().notNullable().references('users.id')
         
       });
